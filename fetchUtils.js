0bc5a272dce8f42fe4cad3fa3a600df52f2a4944
dev-redo <mis05041@naver.com>
2023-01-20 00:55:17 +0900

    Refactor(index, fetchUtils): fetch util 함수를 범용적이게 사용할 수 있게끔 리팩터링

---
+++ fetchUtils.js
@@ -0,0 +1,62 @@
+import fetch from 'node-fetch';
+
+const fetchUtils = () => ({
+  getJSON,
+  getHTML,
+});
+
+const processStatus = (response) => {
+  const successFlag = response.status === 200 || response.ok;
+
+  if (successFlag) {
+    return Promise.resolve(response);
+  }
+
+  return Promise.reject(response);
+};
+
+const defaultJSONHeaders = {
+  Accept: 'application/json',
+};
+
+const defaultHTMLHeaders = {
+  Accept: 'text/html',
+};
+
+const parseJson = (response) => response.json();
+const parseHtml = (response) => response.text();
+
+const getJSON = async ({ url, fetchParams = {}, handleError = {} }) => {
+  const newFetchParams = {
+    ...fetchParams,
+    headers: Object.assign({}, defaultJSONHeaders, fetchParams.headers),
+  };
+
+  return getData(url, newFetchParams)
+    .then((response) => processStatus(response))
+    .then(parseJson)
+    .catch((error) => handleError[error.status]());
+};
+
+const getHTML = async ({ url, fetchParams = {}, handleError = {} }) => {
+  const newFetchParams = {
+    ...fetchParams,
+    headers: Object.assign({}, defaultHTMLHeaders, fetchParams.headers),
+  };
+
+  return getData(url, newFetchParams)
+    .then((response) => processStatus(response))
+    .then(parseHtml)
+    .catch((error) => handleError[error.status]());
+};
+
+const getData = async (url, fetchParams) => {
+  const newFetchParams = {
+    ...fetchParams,
+    method: fetchParams.method ?? 'get',
+  };
+
+  return fetch(url, newFetchParams);
+};
+
+export { fetchUtils };
--- index.js
+++ index.js
@@ -1,21 +1,16 @@
-import fetch from 'node-fetch';
 import { writeFile } from 'fs/promises';
+import { fetchUtils } from './fetchUtils.js';
 
 const url = 'https://school.programmers.co.kr/api/v1/school/challenges/?page=';
 writeProblemFile();
 
 async function writeProblemFile() {
-  try {
-    const problemList = await getSuccessProblemList();
-
-    await writeFile('problems.json', JSON.stringify(problemList));
-  } catch (error) {
-    console.log(error);
-  }
+  const problemList = await getSuccessProblemList();
+  await writeFile('problems.json', JSON.stringify(problemList));
 }
 
 async function getSuccessProblemList() {
-  const { totalPages } = await fetchRequest({ url: url + 1 });
+  const { totalPages } = await fetchUtils().getJSON({ url: url + 1 });
 
   const promisedFetchedDataList = [...new Array(totalPages)].map((_, idx) =>
     fetchProblemPageList(idx + 1)
@@ -26,26 +21,5 @@ async function getSuccessProblemList() {
 }
 
 async function fetchProblemPageList(pageNum) {
-  return (await fetchRequest({ url: url + pageNum })).result;
-}
-
-async function fetchRequest(reqConfig) {
-  const { url, method, headers, body } = reqConfig;
-
-  try {
-    const res = await fetch(url, {
-      method: method || 'GET',
-      headers: headers || {},
-      body: body ? JSON.stringify(body) : undefined,
-    });
-
-    if (!res.ok) {
-      throw new Error('[Pro-Solve] 데이터 fetch하는데 실패했습니다.');
-    }
-
-    const payload = await res.json();
-    return payload;
-  } catch (error) {
-    throw new Error(`${error}`);
-  }
+  return (await fetchUtils().getJSON({ url: url + pageNum })).result;
 }
